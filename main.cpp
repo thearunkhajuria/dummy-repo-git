9b0026f7367ba6898a5d223d131957476dfab648
thearunkhajuria <arunkhajuria121@gmail.com>
2023-02-02 20:50:24 +0530

    Revert "changes"
    This reverts commit 5b69ecb4c0ca3ba9c5fedefbc1fc6f0eec2d473a.

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+//quicksort
+void quicksort(){
+	// quicksort function
+}
+
+void bubbleSort(){
+	// bubblesort function added
+}
+
+void mergeSort(){
+	// merge sort function
+}
+
+void insertionSort(){
+	// merge sort function
+}
+
+int main()
+{
+	cout << "I am Arun" << endl;
+	cout << "I am learning Web development" << endl;
+	cout << "I am doing CSE" << endl;
+}
